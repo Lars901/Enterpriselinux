9a8a01b1f32832c3eeeca4520a7f5627c6111c00
Lars901 <larsove_hoines@outlook.com>
2023-05-16 21:45:39 +0200

    Initial

---
+++ Setup.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+sudo dnf update -y
+sudo dnf upgrade --refresh -y
+sudo dnf config-manager --set-enabled crb
+sudo dnf install -y epel-release
+sudo dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm
+sudo dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-9.noarch.rpm
+sudo dnf config-manager --enable powertools
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate sound-and-video
+
+# Making .config and Moving config files and background to Pictures/Wallpapers
+cd "$builddir" || exit
+mkdir -p "/home/$username/.config"
+mkdir -p "/home/$username/.fonts"
+mkdir -p "/home/$username/Pictures"
+mkdir -p "/home/$username/Wallpapers"
+mkdir -p /usr/share/sddm/themes
+cd $builddir
+#cd /Wallpapers
+#cp -R *.jpg /$HOME/$USER/Pictures/Wallpapers/
+chown -R "$username:$username" "/home/$username"
+
+PKGS=(
+'alsa-utils' # audio utils
+'ark' # compression
+'bash-completion'
+'kio-audiocd' 
+'autoconf' # build
+'automake' # build
+'bash-completion'
+'binutils'
+'bison'
+'bluedevil'
+'bluez'
+'breeze'
+'celluloid' # video players
+'cups' #Common Unix Printing System
+'curl'
+'dialog'
+'dosfstools'
+'flatpak'
+'fonts-terminus'
+'flex'
+'fuse3'
+'fuseiso'
+'gcc'
+'git-core'
+'gdisk'
+'haveged'
+'htop'
+'nftables'
+'openjdk-17-jdk' # Java 17
+'openjdk-17-jre' #Java 17 jre
+'qt5-style-kvantum'
+'libavcodec-extra' # Extra codecs
+'libdvdread ' #Enable dvd playback
+'libnewt-dev'
+'libtool'
+'libdvd-pkg'  #Enable dvd playback
+'libcupsimage2' #Canon Printer driver requirement
+'lsof'
+'lutris'
+'lzop'
+'m4'
+'make'
+'milou'
+'nano'
+'neofetch'
+'ntfs-3g'
+'ntp'
+'okular'
+'netcat-openbsd'
+'os-prober'
+'p7zip'
+'patch'
+'pkgconf' 
+'print-manager'
+'python3-pip'
+'qemu-kvm'
+'libvirt-clients' #Dependecy for quemu
+'libvirt-daemon-system' #Dependecy for quemu
+'bridge-utils' #Dependecy for quemu
+'virtinst' #Dependecy for virt-manager
+'libvirt-daemon' #Dependecy for virt-manager
+'virt-manager'
+'virt-viewer'
+'rsync'
+'systemsettings'
+'fonts-terminus'
+'flatpak'
+'flameshot'
+'traceroute'
+'ufw'
+'unrar'
+'unzip'
+'usbutils'
+'vulkan-tools'
+'wget'
+'kde-zeroconf'
+'zip'
+'g++'
+'libx11-dev'
+'libvirglrenderer1'
+'virgl-server'
+'rpm-build'
+
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo dnf install - y "$PKG"
+done
+systemctl enable cups
+systemctl start cups
+sudo systemctl start libvirtd
+sudo systemctl enable libvirtd
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+#Fonts
+#Requirements
+sudo rpm -i http://mirror.stream.centos.org/9-stream/AppStream/x86_64/os/Packages/xorg-x11-fonts-Type1-7.5-33.el9.noarch.rpm
+sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+sudo dnf makecache
+sudo dnf install fontawesome-fonts.noarch
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d "/home/$username/.fonts"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d "/home/$username/.fonts"
+mv dotfonts/fontawesome/otfs/*.otf "/home/$username/.fonts/"
+chown "$username:$username" "/home/$username/.fonts/*"
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+sudo dnf install -y google-noto-emoji-fonts
+sudo mkdir ~/.config/fontsconfig
+sudo cp 01-emoji.conf ~/.config/fontconfig/conf.d/01-emoji.conf
+# Reloading Font cache
+fc-cache -vf
+
+# Layan Cursors
+cd "$builddir" || exit
+git clone https://github.com/vinceliuice/Layan-cursors
+cd Layan-cursors || exit
+chmod +x ./install.sh
+sudo ./install.sh
+cd "$builddir" || exit
+
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors || exit
+chmod +x ./install.sh
+sudo ./install.sh
+cd "$builddir" || exit
+rm -rf Nordzy-cursors
+
+#Mybash
+cd "$builddir" || exit
+git clone https://github.com/christitustech/mybash.git
+cd /mybash
+sudo chmod +x ./Install.sh
+./Install.sh
+
+#Automatic updates
+sudo dnf install -y dnf-automatic
+
+#Drivers
+wget https://repo.radeon.com/amdgpu-install/22.40.3/rhel/9.1/amdgpu-install-5.4.50403-1.el9.noarch.rpm
+#sudo dnf install -y --accept-eula ./amdgpu-install-5.4.50403-1.el9.noarch.rpm --opencl=rocr,legacy --vulkan=amdvlk,pro
+
+#Qemu
+sudo dnf makecache --refresh
+sudo dnf install -y edk2-ovmf
+ sudo dnf install -y qemu-kvm qemu-img libvirt virt-manager virt-install virt-viewer libvirt-client
+ sudo systemctl start libvirtd*
+ sudo systemctl enable libvirtd
+
+Flatpaks=(
+'Flathub com.github.tchx84.Flatseal' #Flatpak addon
+'flathub com.brave.Browser' #BraveBrowser
+'flathub com.spotify.Client'
+'flathub com.discordapp.Discord'
+'flathub io.github.shiftey.Desktop'
+'flathub com.makemkv.MakeMKV'
+'flathub org.libreoffice.LibreOffice' #Newer version than in repo
+'flathub org.mozilla.Thunderbird' #Email client
+'org.inkscape.Inkscape' #Vector Graphics Editor
+'flathub com.vscodium.codium' #Free/Libre Open Source Software Binaries of VS Code
+)
+
+for PKG in "${Flatpaks[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    flatpak install -y "$PKG"#Flatpaks
+sudo dnf groupinstall -y “KDE Plasma Workspaces”
+sudo dnf --enablerepo=epel group -y install “KDE Plasma Workspaces” “base-x”
+sudo systemctl set-default graphical
+sudo reboot -f
